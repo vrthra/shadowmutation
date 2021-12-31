fb5683699b7b545231e51393188e1f844855731e
Philipp Goerz <dev@philipp-goerz.com>
2021-12-31 14:21:22 +0100

    record wallclock time

--- execute_versions.py
+++ execute_versions.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import json
+import time
 from copy import deepcopy
 from subprocess import run, PIPE, STDOUT
 from collections import defaultdict
@@ -28,11 +29,14 @@ def run_it(path, mode=None, logical_path=None, result_file=None, should_not_prin
 def get_res(path, mode, should_not_print=False):
     res_path = Path('res.json')
     res_path.unlink(missing_ok=True)
+    start = time.time()
     run_res = run_it(path, mode=mode, result_file=res_path, should_not_print=should_not_print)
+    end = time.time()
     try:
         with open(res_path, 'rt') as f:
             res = json.load(f)
             res['exit_code'] = run_res.returncode
+            res['runtime'] = end - start
             # print(res)
             return res
     except FileNotFoundError:
@@ -54,8 +58,14 @@ def extract_data(data):
 
     def line_counts(data):
         return {d[0][1]: d[1] for d in data['subject_count_lines']}
+    
+    def runtime(data):
+        tt = data['runtime']
+        minutes = tt/60
+        seconds = tt%60
+        return f"{minutes:.0f}:{seconds:.2f}"
 
-    return get_sorted(data, 'strong'), get_mode(data), subj_count(data), tool_count(data), line_counts(data)
+    return get_sorted(data, 'strong'), get_mode(data), subj_count(data), tool_count(data), line_counts(data), runtime(data)
 
 
 def main():
@@ -69,6 +79,7 @@ def main():
     mut_ids = []
     subject_ctr = 0
     tool_ctr = 0
+    tool_runtime = 0
     subject_line_ctr = defaultdict(int)
     traditional_results = {'killed': [], 'alive': []}
     for path in sorted(list(Path(args.dir).glob("traditional_*.py"))):
@@ -77,6 +88,7 @@ def main():
         mut_ids.append(mut_id)
         subject_ctr += res['subject_count']
         tool_ctr += res['tool_count']
+        tool_runtime += res['runtime']
         for k, v in res['subject_count_lines']:
             subject_line_ctr[k[1]] += v
         if res['exit_code'] != 0:
@@ -85,32 +97,32 @@ def main():
             traditional_results['alive'].append(mut_id)
     trad_killed = sorted(traditional_results['killed'])
     print("Comparing results:")
-    print(trad_killed, "TRADITIONAL", subject_ctr, tool_ctr)
+    print(trad_killed, "TRADITIONAL", subject_ctr, tool_ctr, f"{tool_runtime/60:.0f}:{tool_runtime%60:.2f}")
 
 
-    split_stream_killed, split_stream_mode, split_stream_subj_count, split_stream_tool_count, ss_line_count = \
+    split_stream_killed, split_stream_mode, split_stream_subj_count, split_stream_tool_count, ss_line_count, ss_runtime = \
         extract_data(get_res(Path(args.dir)/"split_stream.py",     'split'))
-    print(split_stream_killed, split_stream_mode, split_stream_subj_count, split_stream_tool_count)
+    print(split_stream_killed, split_stream_mode, split_stream_subj_count, split_stream_tool_count, ss_runtime)
 
-    modulo_killed, modulo_mode, modulo_subj_count, modulo_tool_count, modulo_line_count = \
+    modulo_killed, modulo_mode, modulo_subj_count, modulo_tool_count, modulo_line_count, modulo_runtime = \
         extract_data(get_res(Path(args.dir)/"split_stream.py",     'modulo'))
-    print(modulo_killed, modulo_mode, modulo_subj_count, modulo_tool_count)
+    print(modulo_killed, modulo_mode, modulo_subj_count, modulo_tool_count, modulo_runtime)
 
-    shadow_killed, shadow_mode, shadow_subj_count, shadow_tool_count, shadow_line_count = \
+    shadow_killed, shadow_mode, shadow_subj_count, shadow_tool_count, shadow_line_count, shadow_runtime = \
         extract_data(get_res(Path(args.dir)/"shadow_execution.py", 'shadow'))
-    print(shadow_killed, shadow_mode, shadow_subj_count, shadow_tool_count)
+    print(shadow_killed, shadow_mode, shadow_subj_count, shadow_tool_count, shadow_runtime)
 
-    shadow_cache_killed, shadow_cache_mode, shadow_cache_subj_count, shadow_cache_tool_count, shadow_cache_line_count = \
+    shadow_cache_killed, shadow_cache_mode, shadow_cache_subj_count, shadow_cache_tool_count, shadow_cache_line_count, shadow_cache_runtime = \
         extract_data(get_res(Path(args.dir)/"shadow_execution.py", 'shadow_cache'))
-    print(shadow_cache_killed, shadow_cache_mode, shadow_cache_subj_count, shadow_cache_tool_count)
+    print(shadow_cache_killed, shadow_cache_mode, shadow_cache_subj_count, shadow_cache_tool_count, shadow_cache_runtime)
 
-    sf_killed, sf_mode, sf_subj_count, sf_tool_count, sf_line_count = \
+    sf_killed, sf_mode, sf_subj_count, sf_tool_count, sf_line_count, sf_runtime = \
         extract_data(get_res(Path(args.dir)/"shadow_execution.py", 'shadow_fork'))
-    print(sf_killed, sf_mode, sf_subj_count, sf_tool_count)
+    print(sf_killed, sf_mode, sf_subj_count, sf_tool_count, sf_runtime)
 
-    sfc_killed, sfc_mode, sfc_subj_count, sfc_tool_count, sfc_line_count = \
+    sfc_killed, sfc_mode, sfc_subj_count, sfc_tool_count, sfc_line_count, sfc_runtime = \
         extract_data(get_res(Path(args.dir)/"shadow_execution.py", 'shadow_fork_cache'))
-    print(sfc_killed, sfc_mode, sfc_subj_count, sfc_tool_count)
+    print(sfc_killed, sfc_mode, sfc_subj_count, sfc_tool_count, sfc_runtime)
 
 
     all_lines = subject_line_ctr.keys() | ss_line_count.keys() | modulo_line_count.keys() | shadow_line_count.keys()
